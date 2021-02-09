d54c1cbcf440cfb0cbee09ce0de6c3e5c754504c
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2021-02-09 22:06:18 +0530

    Update PostgreSQLJDBC_Fetching.java

--- PostgreSQL- Java/src/PostgreSQLJDBC_Fetching.java
+++ PostgreSQL- Java/src/PostgreSQLJDBC_Fetching.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+//Author Swarup
 public class PostgreSQLJDBC_Fetching {
 
 	 public static void main( String args[] ) {
