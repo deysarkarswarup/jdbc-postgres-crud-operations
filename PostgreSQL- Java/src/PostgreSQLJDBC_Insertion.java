fff0c4e8b1e5012e7b67a35f4f8813662a027fbf
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2021-02-09 22:05:38 +0530

    Update PostgreSQLJDBC_Insertion.java

--- PostgreSQL- Java/src/PostgreSQLJDBC_Insertion.java
+++ PostgreSQL- Java/src/PostgreSQLJDBC_Insertion.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+//Author Swarup
 public class PostgreSQLJDBC_Insertion {
 	public static void main(String args[]) {
 	      Connection c = null;
